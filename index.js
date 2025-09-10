8d880ac257e602d78ce9bd51c9abc301e558635c
kittenlover445 <rholland007@outlook.com>
2025-09-09 23:06:11 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import {
+  animate,
+  createDraggable,
+  createSpring,
+  svg,
+  utils,
+} from './node_modules/animejs/lib/anime.esm.js';
+
+
+createDraggable('.goldfrog', {
+  container: 'body',
+  containerFriction: 1,
+
+ 
+});
